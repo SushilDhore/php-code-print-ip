c7ce8bd2b9c1311cc0a565e393a8c8072973267f
SushilDhore <67548707+SushilDhore@users.noreply.github.com>
2023-01-12 17:30:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,18 @@ print("\n");
 print `ifconfig`;
 
 ?>
+
+
+
+#Second Code 
+
+<?php
+#echo '<pre>';
+print("Welcome to Php");
+
+#print("\n");
+
+#print_r (`ifconfig`);
+#print_r(explode(`ifconfig`));
+echo $_SERVER['SERVER_ADDR'];
+?>
